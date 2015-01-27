52b598f35485c63dbd620c9eaab9d8d3d9c0e8e0
JosueGTECH <josue.martinez@gtech.com>
2015-01-27 08:47:54 -0400

    redireccionando a Controller Pendientes desde Home al iniciar la aplicacion

--- PendientesProveedores/Controllers/HomeController.cs
+++ PendientesProveedores/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace PendientesProveedores.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return Redirect("~/Pendientes/Index");
         }
     }
 }
